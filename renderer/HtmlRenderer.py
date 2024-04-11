8c54f0b17c679c6a094c1b6997433fd7a19ffbe9
Thiago Berne <thiago.berne@gmail.com>
2024-04-11 19:05:01 -0300

    Fixing executable generation

--- renderer/HtmlRenderer.py
+++ renderer/HtmlRenderer.py
@@ -1,4 +1,6 @@
 from renderer.JsonFileRenderer import JsonFileRenderer
+import pkgutil
+
 
 class HtmlRenderer(JsonFileRenderer):
     def __init__(self, config):
@@ -6,6 +8,5 @@ class HtmlRenderer(JsonFileRenderer):
 
     def render_string(self, renderer_data) -> str:
         json_string = super().render_string(renderer_data)
-        with open("renderer/ThreadDumpAnalysis.html", "r") as file:
-            html_template = file.read()
-            return html_template.replace("__render_data__", json_string)
+        html_template = pkgutil.get_data(__name__, "ThreadDumpAnalysis.html").decode("utf-8")
+        return html_template.replace("__render_data__", json_string)
